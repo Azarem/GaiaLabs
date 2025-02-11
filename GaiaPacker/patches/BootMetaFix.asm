923b2b0b8b57d2b8340275037aa07b658641cc83
Bryan Moulton <azarem@gmail.com>
2025-02-11 08:13:33 -0500

    Larger boot logo

---
+++ GaiaPacker/patches/BootMetaFix.asm
@@ -0,0 +1,13 @@
+﻿
+------------------------------------------------
+?INCLUDE 'scene_meta'
+------------------------------------------------
+;Fix for spritemap load size (should be a better way to do this)
+
+entry_FB [
+  ppu < #00 >   ;00
+  music < #1B, #00, @bgm_no_music >   ;01
+  bitmap < #00, #10, #10, @gfx_boot_logos, #01 >   ;02
+  palette < #00, #50, #80, @pal_boot_logos >   ;03
+  spritemap < #$0292, #00, @spm_boot_logos >   ;04
+]
