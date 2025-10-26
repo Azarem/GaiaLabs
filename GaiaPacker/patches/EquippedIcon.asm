80bbe91bf792e3120e21045cdf3c02ab18d2d712
Bryan Moulton <azarem@gmail.com>
2025-10-26 04:39:53 -0400

    Minor fix

--- GaiaPacker/patches/EquippedIcon.asm
+++ GaiaPacker/patches/EquippedIcon.asm
@@ -619,3 +619,27 @@ sprite_group_17A483 [
   ] >   ;00
 ]
 
+
+------------------------------------------------------
+?INCLUDE 'pr8F_prologue4'
+------------------------------------------------------
+;Fix for prologue scene 4 to wait before generating sprites
+
+e_pr8F_prologue4 {
+    LDA #$4001
+    TSB $09EC
+    COP [50] ( @palette_1E1B35, #00, #00, #20 )
+    COP [DA] ( #01 )                                  ;this
+    COP [A0] ( @code_0BCF8F, #$0020, #$0020, #$2000 )
+    LDA #$&spritestring_0BD222
+    STA $0026, Y
+    COP [3C] ( @e_pr_thinker_0BD031 )
+    COP [DB] ( #$010D )
+    COP [3C] ( @e_pr_thinker_0BD039 )
+    COP [DA] ( #59 )
+    LDA #$0804
+    STA $064A
+    COP [26] ( #8C, #$0000, #$0000, #00, #$1100 )
+    COP [C1]
+    RTL 
+}
